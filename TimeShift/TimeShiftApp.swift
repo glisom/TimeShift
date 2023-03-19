9b3965c66f8d4c97798b6f20d8f7c038574c5af1
Grant Isom <glisom@icloud.com>
2023-03-19 13:10:24 -0500

    Initial Commit

---
+++ TimeShift/TimeShiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeShiftApp.swift
+//  TimeShift
+//
+//  Created by Grant Isom on 3/19/23.
+//
+
+import SwiftUI
+
+@main
+struct TimeShiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
